b2403e2a177611b158fa5f0fd74ea492731ed536
vihar-61 <vihard@aapoon.net>
2022-01-28 17:04:33 +0530

    day 4 morning

---
+++ Day 3/ctocsprgms/ctocsprgms/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ctocsprgms
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Variable Declaration Section
+            int n, i, j;
+
+            Console.WriteLine("\n----- Welcome To Printing Right Angle Triangle Using Stars -----");
+
+            Console.Write("\n Enter no. of rows to be Printed : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            //Logic Section
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.Write("\n");
+            }
+
+            Console.WriteLine("\n\n ----- Developed by VD © -----");
+            Console.ReadLine();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
